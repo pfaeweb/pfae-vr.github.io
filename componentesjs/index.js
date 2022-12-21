6b1c5f55163b79537763cecf319f79819b110b45
Oscar Rios <oskitah97@gmail.com>
2022-12-21 13:19:36 +0000

    Funcion eliminar texto

--- componentesjs/index.js
+++ componentesjs/index.js
@@ -5,3 +5,13 @@ AFRAME.registerComponent("mylink", {
         })
     }
 })
+
+AFRAME.registerComponent("setdisplay", {
+    init: function() {
+            this.el.addEventListener("mouseenter", (e)=> {
+                this.el.parentNode.removeChild(document.querySelector('a-text'))
+                this.el.parentNode.parentNode.removeChild(document.querySelector('a-image'))
+                this.el.parentNode.parentNode.removeChild(document.querySelector('a-plane'))
+            })
+    }
+})
